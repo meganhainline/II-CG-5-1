34aacbdf43edf2ea8da8326682d7c92ebfbc1262
Megan Hainline <meg.hainline@gmail.com>
2018-08-15 21:46:10 -0500

    change

--- II CG 5-1/Program.cs
+++ II CG 5-1/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 
-namespace II_CG_5_1
+namespace CG_5_1
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Combo();
+            Console.ReadLine();
+        }
+        private static void Combo()
+        {
+            //User inputs first name here
+            Console.WriteLine("Please enter your first name.");
+            string FirstName = Console.ReadLine();
+
+            //User inputs their last name here
+            Console.WriteLine("Please enter your last name.");
+            string LastName = Console.ReadLine();
+
+            //This compiles the input and outputs result
+            Console.WriteLine($"Hello {FirstName} {LastName}! Nice to meet you.");
+            Console.ReadLine();
         }
     }
 }
