fce2604ca9df30f9746345134ce46cba69b9fa66
Megan Hainline <meg.hainline@gmail.com>
2018-08-15 21:34:21 -0500

    Add project files.

---
+++ II CG 5-1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace II_CG_5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
